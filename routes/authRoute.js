9ad5df3b21d3b57ff1782b33b3904fb9e93ff5cb
patipan-patisampita <patipan-patisampita@gmail.com>
2024-11-07 11:47:37 +0700

    update

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,36 +1,37 @@
 import express from 'express'
 import User from '../models/userModel.js'
 import bcrypt from 'bcryptjs'
+import { guestRoute, protectedRoute } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 //Route login page: GET=> http://localhost:3000/login
-router.get('/login', (req, res) => {
+router.get('/login', guestRoute, (req, res) => {
     return res.render('login', { title: 'Login page' })
 })
 
 //Route Register page: GET=> http://localhost:3000/register
-router.get('/register', function (req, res) {
+router.get('/register', guestRoute, function (req, res) {
     return res.render('register', { title: 'Register page' })
 })
 
 //Route Register page: GET=> http://localhost:3000/forgot-password
-router.get('/forgot-password', function (req, res) {
+router.get('/forgot-password', guestRoute, function (req, res) {
     return res.render('forgot-password', { title: 'Forgot-password' })
 })
 
 //Route Register page: GET=> http://localhost:3000/reset-password
-router.get('/reset-password', function (req, res) {
+router.get('/reset-password', guestRoute, (req, res) => {
     return res.render('reset-password', { title: 'Reset password' })
 })
 
 //Route Register page: GET=> http://localhost:3000/profile
-router.get('/profile', function (req, res) {
+router.get('/profile', protectedRoute, (req, res) => {
     return res.render('profile', { title: 'Profile page' })
 })
 
 //Route Register page: POST=> http://localhost:3000/register
-router.post('/register', async (req, res) => {
+router.post('/register', guestRoute, async (req, res) => {
     // console.log(req.body)
     const { name, email, password } = req.body
     try {
@@ -55,7 +56,8 @@ router.post('/register', async (req, res) => {
     }
 })
 
-router.post('/login', async (req, res) => {
+//Route Login page: POST=> http://localhost:3000/login
+router.post('/login', guestRoute, async (req, res) => {
     // console.log(req.body)
     const { email, password } = req.body
     try {
@@ -73,6 +75,12 @@ router.post('/login', async (req, res) => {
         req.flash('error', 'Something went wrong, try again!')
         return req.redirect('/login')
     }
+})
+
+//Handle user logout
+router.post('/logout', (req, res) => {
+    req.session.destroy()
+    return res.redirect('/login')
 })
 
 export default router
